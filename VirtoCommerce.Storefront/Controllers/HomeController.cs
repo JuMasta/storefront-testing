42b9408b66d7eeb24ccefaa3dfd9334e5ac1a8f3
Никита Булатов <n.bulatov@innopolis.ru>
2022-03-10 15:50:56 +0300

    first commit

---
+++ VirtoCommerce.Storefront/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using VirtoCommerce.Storefront.Infrastructure;
+
+namespace VirtoCommerce.Storefront.Controllers
+{
+    [StorefrontRoute]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+    }
+}
